e85a114b6478a9830b2c4e1bc4a26fea3091133b
Shougo Matsushita <Shougo.Matsu@gmail.com>
2022-02-05 08:49:45 +0900

    Initial version

---
+++ README.md
@@ -0,0 +1,28 @@
+# ddu-filter-matcher_hidden
+
+Hide hidden files matcher for ddu.vim
+
+This matcher filters hidden files.
+
+## Required
+
+### denops.vim
+
+https://github.com/vim-denops/denops.vim
+
+### ddu.vim
+
+https://github.com/Shougo/ddu.vim
+
+## Configuration
+
+```vim
+" Use the filter.
+call ddu#custom#patch_global({
+    \   'sourceOptions': {
+    \     '_': {
+    \       'matchers': ['matcher_hidden'],
+    \     },
+    \   }
+    \ })
+```
---
+++ denops/@ddu-filters/matcher_hidden.ts
@@ -0,0 +1,29 @@
+import { BaseFilter, DduItem, SourceOptions } from "https://deno.land/x/ddu_vim@v0.7.0/types.ts";
+import { Denops } from "https://deno.land/x/ddu_vim@v0.7.0/deps.ts";
+import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.1.0/file.ts#^";
+import { basename } from "https://deno.land/std@0.123.0/path/mod.ts";
+
+type Params = Record<never, never>;
+
+export class Filter extends BaseFilter<Params> {
+  async filter(args: {
+    denops: Denops;
+    sourceOptions: SourceOptions;
+    input: string;
+    items: DduItem[];
+  }): Promise<DduItem[]> {
+    return Promise.resolve(args.items.filter(
+      (item) => {
+        const action = item.action as ActionData;
+        if (!action.path) { return false; }
+        return args.input.includes(".") ||
+          (!basename(action.path).startsWith(".") &&
+           !item.matcherKey.startsWith("."));
+      }
+    ));
+  }
+
+  params(): Params {
+    return {};
+  }
+}
