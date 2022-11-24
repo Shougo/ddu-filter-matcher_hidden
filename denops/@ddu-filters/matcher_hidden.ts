ffbd3a300d7780379c8bcdf4437680d49f3da9b1
Shougo Matsushita <Shougo.Matsu@gmail.com>
2022-11-24 19:51:45 +0900

    Fix version

--- denops/@ddu-filters/matcher_hidden.ts
+++ denops/@ddu-filters/matcher_hidden.ts
@@ -1,5 +1,5 @@
-import { BaseFilter, DduItem, SourceOptions } from "https://deno.land/x/ddu_vim@v0.14/types.ts";
-import { Denops } from "https://deno.land/x/ddu_vim@v0.14/deps.ts";
+import { BaseFilter, DduItem, SourceOptions } from "https://deno.land/x/ddu_vim@v2.0.0/types.ts";
+import { Denops } from "https://deno.land/x/ddu_vim@v2.0.0/deps.ts";
 import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.3.1/file.ts";
 import { basename, dirname } from "https://deno.land/std@0.165.0/path/mod.ts";
 
