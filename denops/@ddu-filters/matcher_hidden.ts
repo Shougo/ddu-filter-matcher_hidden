54e8c6206edd8892438c283f6d60a5984d6f4b3b
Shougo Matsushita <Shougo.Matsu@gmail.com>
2023-06-10 07:41:21 +0900

    Update

--- denops/@ddu-filters/matcher_hidden.ts
+++ denops/@ddu-filters/matcher_hidden.ts
@@ -1,13 +1,16 @@
-import { BaseFilter, DduItem, SourceOptions } from "https://deno.land/x/ddu_vim@v2.0.0/types.ts";
-import { Denops } from "https://deno.land/x/ddu_vim@v2.0.0/deps.ts";
-import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.3.2/file.ts";
-import { basename, dirname } from "https://deno.land/std@0.166.0/path/mod.ts";
+import {
+  BaseFilter,
+  DduItem,
+  SourceOptions,
+} from "https://deno.land/x/ddu_vim@v3.0.2/types.ts";
+import { Denops } from "https://deno.land/x/ddu_vim@v3.0.2/deps.ts";
+import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.5.0/file.ts";
+import { basename, dirname } from "https://deno.land/std@0.191.0/path/mod.ts";
 
 type Params = Record<never, never>;
 
 export class Filter extends BaseFilter<Params> {
-  // deno-lint-ignore require-await
-  override async filter(args: {
+  override filter(args: {
     denops: Denops;
     sourceOptions: SourceOptions;
     input: string;
@@ -16,12 +19,12 @@ export class Filter extends BaseFilter<Params> {
     return Promise.resolve(args.items.filter(
       (item) => {
         const action = item.action as ActionData;
-        if (!action.path) { return false; }
+        if (!action.path) return false;
         return args.input.includes(".") ||
           (!basename(action.path).startsWith(".") &&
-           !basename(dirname(action.path)).startsWith(".") &&
-           !item.matcherKey.startsWith("."));
-      }
+            !basename(dirname(action.path)).startsWith(".") &&
+            !item.matcherKey.startsWith("."));
+      },
     ));
   }
 
